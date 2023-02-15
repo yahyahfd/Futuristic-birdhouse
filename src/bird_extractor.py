10835ffb50ed206bafca6e56c54c25611c5e9291
MEZIANE reda <reda.meziane@etu.univ-paris-diderot.fr>
2023-02-15 21:08:32 +0100

    réecriture de closest_color, ajout de nouvelles couleurs

--- src/bird_extractor.py
+++ src/bird_extractor.py
@@ -15,15 +15,27 @@ def extract_bird(img1, img2):
     cv2.imwrite("res/ResultBird.png", result)
 
 
-def closest_color(color, color_list):
+# def closest_color(color, color_list):
+#    min_distance = np.inf
+#    res = None
+#    for c in color_list:
+#        distance = np.sum(np.abs(c - color))
+#        if distance < min_distance:
+#            min_distance = distance
+#            res = c
+#    return res
+
+def closest_color(pixel, color_list):
     min_distance = np.inf
-    res = None
-    for c in color_list:
-        distance = np.sum(np.abs(c - color))
+    closest = None
+    for color in color_list:
+        b, g, r = pixel
+        cr, cg, cb = color
+        distance = ((r - cr) ** 2 + (g - cg) ** 2 + (b - cb) ** 2) ** 0.5
         if distance < min_distance:
             min_distance = distance
-            res = c
-    return res
+            closest = color
+    return closest
 
 
 # renvoie la couleur la plus dominante a partir de l'image de l'oiseau
@@ -41,14 +53,20 @@ def get_dominant_color(image, colors_list):
                 color_counts[color_key] += 1
             else:
                 color_counts[color_key] = 1
-    return max(color_counts, key=color_counts.get)
+    # return max(color_counts, key=color_counts.get)
+    return color_counts
 
 
 # les différentes couleurs possibles
-colors = [np.array([0, 0, 0], dtype=np.int32),
-          np.array([255, 0, 0], dtype=np.int32),
-          np.array([0, 255, 0], dtype=np.int32),
-          np.array([0, 0, 255], dtype=np.int32)]
+colors = [np.array([0, 0, 0], dtype=np.int32),  # Noir
+          np.array([255, 0, 0], dtype=np.int32),  # Rouge
+          np.array([0, 255, 0], dtype=np.int32),  # Vert
+          np.array([255, 255, 0], dtype=np.int32),  # Jaune
+          np.array([255, 165, 0], dtype=np.int32),  # Orange
+          np.array([255, 255, 255], dtype=np.int32),  # Blanc
+          np.array([150, 75, 0], dtype=np.int32),  # Marron
+          np.array([128, 128, 128], dtype=np.int32),  # Gris
+          np.array([0, 0, 255], dtype=np.int32)]  # Bleu
 
-extract_bird("res/Background.png", "res/Bird.png")
+extract_bird("res/Background.png", "res/Rouge.png")
 print(get_dominant_color("res/ResultBird.png", colors))
