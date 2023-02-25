da417faede8f06a5e04998bb2d8dc8f50b430bd2
hafid <hafidyahya1999@gmail.com>
2023-02-25 14:14:35 +0100

    on passe rgb à hsv désormais pour mieux regrouper les couleurs: J'ai gardé les anciennes méthodes si jamais on a besoin de les réutiliser (rapprochement de couleurs etc). Notre façon de calculer l'aire est correcte, mais la distance de l'objectif ne doit en aucun cas etre tout le temps la même. Nous devons utiliser une sorte de capteur de distance ultra son afin de avoir une valeur plus logique

--- src/bird_extractor.py
+++ src/bird_extractor.py
@@ -80,37 +80,70 @@ def get_dominant_color(image, colors_list):
             else:
                 color_counts[color_key] = 1
 
+# sert à convertir une couleur rgb en hsv
+def rgb_to_hsv(color):
+    return cv2.cvtColor(color, cv2.COLOR_RGB2HSV)
 
-# compte le nombre de pixels de chaque couleur
-def color_count(img, colors_list,dir):
-    print(f"color_count: file = {img}")
+# compte le nombre de pixels de chaque couleur en rgb
+# puis converti le tout en hsv avant de regrouper
+# les couleurs par leur hue (teinte)
+def color_count_dict(img,dir):
+    # print(f"color_count_dict: file = {img}")
     image = cv2.imread(dir + img)
     resized_img = cv2.resize(image, (300, 200))
-    color_counts = {color: 0 for color in colors_list.keys()}
     total_pixels = 0
+    color_counts = {}
     for i in range(len(resized_img)):
         for j in range(len(resized_img[0])):
-            if not np.all(resized_img[i][j] == [255, 255, 255]):
-                closest = closest_color(resized_img[i][j], colors_list.values())
-                for color, value in colors_list.items():
-                    if np.array_equal(value, closest):
-                        color_counts[color] += 1
-                        total_pixels += 1
-    # print(f"area: {total_pixels} pixels")
+            rgb_color = resized_img[i][j]
+            if not np.all(rgb_color == [255, 255, 255]):
+                total_pixels +=1
+                hsv_color = cv2.cvtColor(np.uint8([[rgb_color]]), cv2.COLOR_RGB2HSV)[0][0]
+                hue = hsv_color[0]
+                if(hue in color_counts):
+                    color_counts[hue] +=1
+                else:
+                    color_counts[hue] = 1
+    # max = 0
     for color, count in color_counts.items():
         color_counts[color] = round((count / total_pixels) * 100, 2)
         # print(f"{color}: {round((count / total_pixels) * 100, 2)}")
+        # if(color_counts[color]>max):
+            # max = color_counts[color]
+    # print(max)
     return color_counts
 
+
+# def color_count(img, colors_list,dir):
+#     color_count_dict(img,dir)
+#     print(f"color_count: file = {img}")
+#     image = cv2.imread(dir + img)
+#     resized_img = cv2.resize(image, (300, 200))
+#     color_counts = {color: 0 for color in colors_list.keys()}
+#     total_pixels = 0
+#     for i in range(len(resized_img)):
+#         for j in range(len(resized_img[0])):
+#             if not np.all(resized_img[i][j] == [255, 255, 255]):
+#                 closest = closest_color(resized_img[i][j], colors_list.values())
+#                 for color, value in colors_list.items():
+#                     if np.array_equal(value, closest):
+#                         color_counts[color] += 1
+#                         total_pixels += 1
+#     # print(f"area: {total_pixels} pixels")
+#     for color, count in color_counts.items():
+#         color_counts[color] = round((count / total_pixels) * 100, 2)
+#         # print(f"{color}: {round((count / total_pixels) * 100, 2)}")
+#     return color_counts
+
 # les différentes couleurs possibles
-colors = {"Noir": np.array([0, 0, 0], dtype=np.int32),
-          "Rouge": np.array([255, 0, 0], dtype=np.int32),
-          "Vert": np.array([0, 255, 0], dtype=np.int32),
-          "Jaune": np.array([255, 255, 0], dtype=np.int32),
-          "Blanc": np.array([255, 255, 255], dtype=np.int32),
-          "Bleu": np.array([0, 0, 255], dtype=np.int32),
-          "Marron": np.array([150, 75, 255], dtype=np.int32),
-          "Cyan": np.array([0, 255, 255], dtype=np.int32)}
+# colors = {"Noir": np.array([0, 0, 0], dtype=np.int32),
+#           "Rouge": np.array([255, 0, 0], dtype=np.int32),
+#           "Vert": np.array([0, 255, 0], dtype=np.int32),
+#           "Jaune": np.array([255, 255, 0], dtype=np.int32),
+#           "Blanc": np.array([255, 255, 255], dtype=np.int32),
+#           "Bleu": np.array([0, 0, 255], dtype=np.int32),
+#           "Marron": np.array([150, 75, 255], dtype=np.int32),
+#           "Cyan": np.array([0, 255, 255], dtype=np.int32)}
 
 
 # extrait tout les oiseaux dans le fichier birds
@@ -124,15 +157,15 @@ def extract_all():
 
 
 # applique color_count pour tout les fichiers dans le dossier results
-def color_count_all(color_list):
+def color_count_all():
     directory = "res/results/"
     for file in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, file)) and not file.startswith('.'):
-            color_count(file, color_list,directory)
+            color_count_dict(file,directory)
 
 def main():
     extract_all()
-    color_count_all(colors)
+    color_count_all()
 
 if __name__ == "__main__":
     main()
